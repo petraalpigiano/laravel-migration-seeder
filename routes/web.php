861f3768eb21373f5d3ac34bbba252417eaab8fb
petraalpigiano <petraalpigiano@gmail.com>
2025-08-24 17:30:28 +0200

    add model

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [PageController::class, 'index'])->name('home');
 
 Route::get('/contact', function () {
     return view('contact');

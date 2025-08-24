dd690f595e647d6aa0a8cdf7272164a088d61ee3
petraalpigiano <petraalpigiano@gmail.com>
2025-08-24 16:52:46 +0200

    add migration batch 3

--- database/migrations/2025_08_24_144405_add_arrival_time_to_trains_table.php
+++ database/migrations/2025_08_24_144405_add_arrival_time_to_trains_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('trains', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('trains', function (Blueprint $table) {
+            $table->time('arrival_time');
         });
     }
 
@@ -22,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('trains');
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('arrival_time');
+        });
     }
 };
--- database/migrations/2025_08_24_144805_add_is_punctual_to_trains_table.php
+++ database/migrations/2025_08_24_144805_add_is_punctual_to_trains_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('trains', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('trains', function (Blueprint $table) {
+            $table->boolean('is_punctual');
         });
     }
 
@@ -22,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('trains');
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('is_punctual');
+        });
     }
 };

7e5bb7a7e6cafb11df11f32a74c03c2acbe2e027
petraalpigiano <petraalpigiano@gmail.com>
2025-08-25 11:35:19 +0200

    add run method

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -13,5 +13,23 @@ class TrainsTableSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(Faker $faker): void {}
+    public function run(Faker $faker): void
+    {
+
+        for ($i = 0; $i < 20; $i++) {
+            $newtrain = new Train();
+
+            $newtrain->company = $faker->company();
+            $newtrain->departure = $faker->city();
+            $newtrain->arrival = $faker->city();
+            $newtrain->departure_time = $faker->dateTimeBetween('-1 month', '+1 month');
+            $newtrain->arrival_time = $faker->dateTimeBetween('-1 month', '+1 month');
+            $newtrain->train_code = $faker->randomNumber(5, true);
+            $newtrain->number_carriages = $faker->randomDigitNotNull();
+            $newtrain->is_punctual = $faker->numberBetween(0, 1);
+            $newtrain->is_cancelled = $faker->numberBetween(0, 1);
+
+            $newtrain->save();
+        }
+    }
 }

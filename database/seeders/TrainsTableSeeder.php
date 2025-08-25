286a92c1367a563bb5ff471338bc611c3aa0651c
petraalpigiano <petraalpigiano@gmail.com>
2025-08-25 11:15:32 +0200

    add seeder

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void {}
+}
